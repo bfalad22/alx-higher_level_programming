5701574499cb8e8e041ef600ee751f3e436c9e27
bfalad22 <bolasaliu93@gmail.com>
2023-04-13 04:00:12 -0700

    Integer Validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Class that defines the attributes of Geometric Shapes """
+
+
+class BaseGeometry:
+    """ Class that defines the attributes of Geometric Shapes """
+
+    def area(self):
+        """ Method that defines the area of a geometric shape """
+
+        raise Exception("area() is not implemneted")
+
+    def integer_validator(self, name, value):
+        """ Method that receives the value property
+
+
+        Args:
+            name: name of the object
+            value: value of the property
+
+
+        """
+
+
+        if type(value) is not int:
+            raise TyeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
