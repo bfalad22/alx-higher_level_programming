79b12d0ddb90f1e74894f22f8698e8d4b4215823
bfalad22 <bolasaliu93@gmail.com>
2023-04-15 00:24:00 -0700

    Eval is Magic

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -4,48 +4,49 @@
 
 This module is composed by a class that defines a Rectangle
 
+
 """
+
 
 class Rectangle:
     """ Class that defines a rectangle """
 
     def __init__(self, width=0, height=0):
-
-        """Method that initializes the instance
-
+        """ Method that initializes the instance
 
         Args:
-        width:rectangle width
-        height:rectangle height
+            width: rectangle width
+            height: rectangle height
+
 
         """
-
 
         self.width = width
         self.height = height
 
     @property
-    def width (self):
+    def width(self):
         """ method that returns the value of the width
 
         Returns:
-        rectangle width
+            rectangle width
+
 
         """
 
-        return self.--width
+        return self.__width
 
     @width.setter
-    def width (self, value):
-
+    def width(self, value):
         """ method that defines the width
 
         Args:
-        value: width
+            value: width
 
         Raises:
-        TypeError:if width is not an integer
-        ValueError:if width is less than zero
+            TypeError: if width is not an integer
+            ValueError: if width is less than zero
+
 
         """
 
@@ -55,20 +56,20 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-        @property
+    @property
     def height(self):
-
         """ method that returns the value of the height
 
         Returns:
             rectangle height
+
+
         """
 
         return self.__height
 
     @height.setter
     def height(self, value):
-
         """ method that defines the height
 
         Args:
@@ -77,6 +78,8 @@ class Rectangle:
         Raises:
             TypeError: if height is not an integer
             ValueError: if height is less than zero
+
+
         """
 
         if not isinstance(value, int):
@@ -86,21 +89,23 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-
         """ Method that calculates the Rectangle area
 
         Returns:
             rectangle area
+
+
         """
 
         return self.width * self.height
 
     def perimeter(self):
-
         """ Method that calculates the Rectangle perimeter
 
         Returns:
             rectangle perimeter
+
+
         """
 
         if self.width == 0 or self.height == 0:
@@ -109,11 +114,11 @@ class Rectangle:
         return (2 * self.width) + (2 * self.height)
 
     def __str__(self):
-
         """ Method that returns the Rectangle #
 
         Returns:
             str of the rectangle
+
         """
 
         rectangle = ""
@@ -127,11 +132,11 @@ class Rectangle:
         return rectangle[:-1]
 
     def __repr__(self):
-
         """ Method that returns the string represantion of the instance
 
         Returns:
             string represenation of the object
+
         """
 
         return "Rectangle({:d}, {:d})".format(self.width, self.height)
