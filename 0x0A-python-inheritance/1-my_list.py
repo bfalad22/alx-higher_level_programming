94074bfaa776b6c548cc6d20b2359af2396d2118
bfalad22 <bolasaliu93@gmail.com>
2023-04-14 22:28:01 -0700

    My list Updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
-"""Class that inherits the attributes references of class list
-
+""" Class that inherits the attributes references of class list
 
     Args:
-        list:class list
+        list: class list
 
-"""
+    """
 
-    class MyList(list):
-        """Class that inherits the attributes references of class list
 
-        Args:
-            list:class list
-        """
+class MyList(list):
+    """ Class that inherits the attributes references of class list
 
-        def print_sorted(self):
-            """Method that prints the sorted list"""
-            l_sorted = self.copy()
-            l_sorted.sort()
-            print(l_sorted)
+    Args:
+        list: class list
+
+    """
+
+    def print_sorted(self):
+        """ Method that prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
