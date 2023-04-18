a550dc20d8551d1960fe3d3c0559494d0bf67679
bfalad22 <bolasaliu93@gmail.com>
2023-04-18 05:01:52 -0700

    Display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,51 @@
 #!/usr/bin/python3
 
-""" 3-main """
-
 """ Module that contains class Rectangle,
 inheritance of class Base
 """
 from models.base import Base
 
-        @property
+
+class Rectangle(Base):
+    """ Class Rectangle """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes instances """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
     def x(self):
         """ x getter """
         return self.__x
@@ -38,4 +76,13 @@ from models.base import Base
     def area(self):
         """ returns the area of the rectangle object """
         return self.width * self.height
+
+    def display(self):
+        """ displays a rectangle """
+        rectangle = self.y * "\n"
+        for i in range(self.height):
+            rectangle += (" " * self.x)
+            rectangle += ("#" * self.width) + "\n"
+
+        print(rectangle, end='')
 
