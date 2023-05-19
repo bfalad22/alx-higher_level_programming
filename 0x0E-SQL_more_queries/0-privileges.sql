96bda6f886c4ea8d9b56cea60081e4f9c935ccdf
bfalad22 <bolasaliu93@gmail.com>
2023-05-19 14:22:27 -0700

    Priviledges of SQL

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Query that lists all priviledges of the MySQL users user_0d_1 and user_0d_2
+SHOWS GRANTS FOR 'user_0d_1'@'localhost';
+SHOWS GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This Readme is for 0x0E-SQL_more_queries 
