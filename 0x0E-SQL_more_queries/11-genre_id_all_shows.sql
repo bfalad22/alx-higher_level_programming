65e187ff56f53bc5713a5f255d69fac7fab4ea4e
bfalad22 <bolasaliu93@gmail.com>
2023-05-20 22:10:40 -0700

    SQL Queries

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre.id
 -- If a show doesnt have a genre, display NULL
 
-SELECT tv_shows.title, tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
