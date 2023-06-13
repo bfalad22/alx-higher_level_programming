4f3a6a18dec3d5c6eee27d90197c3768dac70ac0
bfalad22 <bolasaliu93@gmail.com>
2023-06-13 10:57:26 -0700

    JavaScript Warmup

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let num = process.argv[2];
+num = Number(num);
+for (let i = 0; i < num; i++) {
+  console.log('C is fun');
+}
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const firstNum = Number(process.argv[2]);
+const secondNum = Number(process.argv[3]);
+// if (isNaN(firstNum) || isNaN(secondNum)) {
+// console.log('NaN');
+// }
+
+add(firstNum, secondNum);
+
+function add (a, b) {
+  console.log(a + b);
+}
