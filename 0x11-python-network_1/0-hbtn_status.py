8f56b8eed020b2369f637f01aaca3fc751819dd3
bfalad22 <bolasaliu93@gmail.com>
2023-07-06 07:48:03 -0700

    Python Network 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urllib package
-"""
+"""fetch url with urllib"""
+from urllib.request import urlopen
 
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+def status():
+    """fetch url body"""
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        convert = body.decode('utf-8')
+        print('Body response:\n\t- type: {}'.format(type(body)))
+        print('\t- content: {}\n\t- utf8 content: {}'.
+                format(body, convert, end=''))
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
 
-print(f'- type: {type(body)}')
-print(f'- content: {body}')
+if  __name__ == '__main__':
+    status()
