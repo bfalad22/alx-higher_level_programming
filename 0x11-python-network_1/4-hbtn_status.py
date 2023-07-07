1fe0826ba78c88643a99e83da6b46cf5aacedd76
bfalad22 <bolasaliu93@gmail.com>
2023-07-06 19:32:08 -0700

    Python Network 1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Same as Task 0 but using request package
-"""
-
+"""initialize"""
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+
+req = requests.get('https://alx-intranet.hbtn.io/status')
+print('Body response:\n\t- type: {}\n\t- content: {}'.
+      format(type(req.text), req.text))
